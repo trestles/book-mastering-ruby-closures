29dd442b44711186d3b2157894bc8e2305b72319
jt <jon@arclocal.com>
2018-12-27 06:24:20 -0800

    some copy
    some fixes from final
    
    added some copy

---
+++ ch04-building-your-own-lazy-enumerables.rb
@@ -0,0 +1,66 @@
+=begin
+
+
+Implementing Lax p63
+
+Implementing Lazy map() p66
+  "Lets implement method chaining on methods such as map() and take(). Enumerable::Lax returns a 
+   Lax instance which means we need to define Lax versions of map() and take(). Each invocation of map()
+   and take() will in turn return yet another Lax instance
+  "
+
+Implementing Lazy take() p67
+
+=end
+
+
+module Enumerable
+    def lax
+      Lax.new(self)
+    end
+  end
+  
+  class Lax < Enumerator
+  
+    def initialize(receiver)
+      super() do |yielder|
+        begin
+          receiver.each do |val|
+            if block_given?
+              yield(yielder, val)
+            else
+              yielder << val
+            end
+          end
+        rescue StopIteration
+        end
+      end
+    end
+  
+    def map(&block)
+      Lax.new(self) do |yielder, val|
+        yielder << block.call(val)
+      end
+    end
+  
+    def take(n)
+      taken = 0
+      Lax.new(self) do |yielder, val|
+        if taken < n
+          yielder << val
+          taken += 1
+        else
+          raise StopIteration
+        end
+      end
+    end
+  end
+
+
+
+1.upto(Float::INFINITY)
+  .lax
+  .map { |x| x*x }
+  .map { |x| x+1 }
+  .take(5)
+  .to_a
